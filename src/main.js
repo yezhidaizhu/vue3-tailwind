2861d6652b44623c45a1015f6f91751db5ac4093
nofish <2580938905@qq.com>
2021-07-21 22:00:56 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// tailwind
+import "./style.css"
+
+createApp(App).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
+}
